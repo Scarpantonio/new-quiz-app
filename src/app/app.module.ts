ad933d8018138ac1836e1ca1ffe03d7f8eb75e23
Scarpantonio <scarpantonioc@gmail.com>
2020-10-23 08:56:07 -0600

    UPDATES - components and services added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 //By passing HttpClientModule in the imports property of the object given to @NgModule, you’re telling Angular to make the HttpClientModule available.
-import { HttpClientModule } from '@angular/common/http';
-import { QuestionFormComponent } from './question-form/question-form.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
+import { QuestionFormComponent } from "./question-form/question-form.component";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [AppComponent, HttpClientModule, QuestionFormComponent],
-  imports: [BrowserModule, ReactiveFormsModule],
+  declarations: [AppComponent, QuestionFormComponent],
+  imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
